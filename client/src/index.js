dd02e6b12dc765400f6322963414c3d28e425e79
Jaivignesh-Jv <jaivignesh_jv@cstream.io>
2023-10-24 15:17:12 +0530

    routerfix

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import Login from "./Login";
 import Notfound from "./components/Notfound";
 
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Login />,
